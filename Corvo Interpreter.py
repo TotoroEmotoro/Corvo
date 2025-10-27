7e7d6555992974101c7ec767971da11fe7604e9f
Austin <140423839+TotoroEmotoro@users.noreply.github.com>
2025-10-27 13:14:11 +0000

    Update copyright and licensing information

--- Corvo Interpreter.py
+++ Corvo Interpreter.py
@@ -1,20 +1,13 @@
-# Corvo - a lightweight interpreted programming language using Lark
-# Copyright (C) 2025  Austin (GitHub: TotoroEmotoro)
+# Corvo Language and Runtime
+# Copyright (c) 2025 Austin (GitHub: TotoroEmotoro)
+# Licensed under the Corvo Source-Available License, Version 1.0.
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 3 of the License, or (at your option) any later version.
+# Permission is granted to use Corvo to write and distribute your own programs.
+# You may view and study the Corvo source code for educational purposes.
+# You may NOT modify or redistribute Corvo itself without written permission.
 #
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this library; if not, see <https://www.gnu.org/licenses/>.
-#
-# Contact: https://github.com/TotoroEmotoro/Corvo
+# See the LICENSE file for full license text.
+
 
 
 from lark import Lark, Transformer
